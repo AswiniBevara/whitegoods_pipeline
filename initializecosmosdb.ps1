0b8294e30fc9ee9bef2b1964fc52f76a79a2799d
komali <komalio@sysgain.com>
2018-04-18 21:24:38 +0530

    Update initializecosmosdb.ps1

--- initializecosmosdb.ps1
+++ initializecosmosdb.ps1
@@ -4,59 +4,64 @@
 #datapacketUri="https://datapacketdqdpoolvbrlfs.azurewebsites.net"														#Update this Value
 #deviceManagementUri="https://devicemanagementdqdpoolvbrlfs.azurewebsites.net"											#Update this Value
 param(
-[string] $tenantId = "$1",
-[string] $clientId = "$2",
-[string] $clientSecret = "$3",
-[string] $datapacketUri = "$4",
-[string] $deviceManagementUri = "$5"
-)
-
-function Get-AccessTokenAPI{
-    Write-Host "`nRequesting access token.." -ForegroundColor Green
-
-    $tokenParams = @{
-	  client_id=$clientId;
-  	  client_secret=$clientSecret;
-      resource=$clientId;
-	  grant_type='client_credentials';
-	  scope='openid'
-	}
-
-    $baseUri="https://login.microsoftonline.com/"+$tenantId+"/oauth2/token"
-    $response=Invoke-RestMethod -Uri $baseUri -Method POST -Body $tokenParams
- 
-    return $response.access_token
-}
-function CosmosDbInit{
-param([string]$apiBaseURL,[string]$accessToken)
-
-    Write-Host "`nInitializing Cosmos DB..." -ForegroundColor Green
-    $APIURL=[string]::Format("{0}/api/templates/generate",$apiBaseURL)
-    $result=Invoke-RestMethod -Uri $APIURL -Method Post -ContentType "application/json" -Headers @{ "Authorization" = "Bearer $accessToken" }
-    if($result -eq "true")
-    {
-        Write-Host "`nCosmos DB initialized successfully..!" -ForegroundColor Green    
-        return $true;
+    [string] $tenantId = "$1",
+    [string] $clientId = "$2",
+    [string] $clientSecret = "$3",
+    [string] $datapacketUri = "$4",
+    [string] $deviceManagementUri = "$5"
+    )
+    Install-Module AzureRM -Force
+    Import-Module AzureRM 
+    Install-Module AzureADPreview -Force
+    Import-Module AzureADPreview 
+    Login-AzureRmAccount 
+    Select-AzureRmSubscription -SubscriptionName "Sysgain-Backup" -TenantID $tenantId
+    function Get-AccessTokenAPI{
+        Write-Host "`nRequesting access token.." -ForegroundColor Green
+    
+        $tokenParams = @{
+          client_id=$clientId;
+            client_secret=$clientSecret;
+          resource=$clientId;
+          grant_type='client_credentials';
+          scope='openid'
+        }
+    
+        $baseUri="https://login.microsoftonline.com/"+$tenantId+"/oauth2/token"
+        $response=Invoke-RestMethod -Uri $baseUri -Method POST -Body $tokenParams
+     
+        return $response.access_token
     }
-    else
-    {
-        Write-Host "`nError !! Cosmos DB not initialized!" -ForegroundColor Red  
-        return $false;  
+    function CosmosDbInit{
+    param([string]$apiBaseURL,[string]$accessToken)
+    
+        Write-Host "`nInitializing Cosmos DB..." -ForegroundColor Green
+        $APIURL=[string]::Format("{0}/api/templates/generate",$apiBaseURL)
+        $result=Invoke-RestMethod -Uri $APIURL -Method Post -ContentType "application/json" -Headers @{ "Authorization" = "Bearer $accessToken" }
+        if($result -eq "true")
+        {
+            Write-Host "`nCosmos DB initialized successfully..!" -ForegroundColor Green    
+            return $true;
+        }
+        else
+        {
+            Write-Host "`nError !! Cosmos DB not initialized!" -ForegroundColor Red  
+            return $false;  
+        }
     }
-}
-        $datapacketUriOIDC=$datapacketUri+"/signin-oidc"      
-        $deviceManagementUriOIDC=$deviceManagementUri+"/signin-oidc"   
-        $replyURLList = @($datapacketUriOIDC,$deviceManagementUriOIDC);  
-        Write-Host '', 'Configuring and setting the Azure AD reply URLs' -ForegroundColor Green
-        #####################################################################
-        # Get Access token for calling API
-        #####################################################################
-        $accessToken=Get-AccessTokenAPI $tenantId $clientId $clientSecret
-        Write-Host "Access token is " $accessToken -ForegroundColor Magenta
-        #add sleep inorder to have API startup successfully
-        Start-Sleep -s 10
-        #####################################################################
-        # Call API to initialize cosmos DB with default starter IoT templates
-        # This templates will be loaded in the data packet designer
-        #####################################################################
-        $cosmosDBInitResult=CosmosDbInit $datapacketUri $accessToken  
+            $datapacketUriOIDC=$datapacketUri+"/signin-oidc"      
+            $deviceManagementUriOIDC=$deviceManagementUri+"/signin-oidc"   
+            $replyURLList = @($datapacketUriOIDC,$deviceManagementUriOIDC);  
+            Write-Host '', 'Configuring and setting the Azure AD reply URLs' -ForegroundColor Green
+            #####################################################################
+            # Get Access token for calling API
+            #####################################################################
+            $accessToken=Get-AccessTokenAPI $tenantId $clientId $clientSecret
+            Write-Host "Access token is " $accessToken -ForegroundColor Magenta
+            #add sleep inorder to have API startup successfully
+            Start-Sleep -s 10
+            #####################################################################
+            # Call API to initialize cosmos DB with default starter IoT templates
+            # This templates will be loaded in the data packet designer
+            #####################################################################
+            $cosmosDBInitResult=CosmosDbInit $datapacketUri $accessToken  
