02b4f5900af4d11bdd96e06afe89962014586aa3
komali <komalio@sysgain.com>
2018-06-21 17:19:57 +0530

    Update initializecosmosdb.ps1

--- scripts/initializecosmosdb.ps1
+++ scripts/initializecosmosdb.ps1
@@ -80,26 +80,20 @@ workflow  container{
             return $false;  
         }
     }
-            #####################################################################
             # Update Azure AD applications reply urls
-            #####################################################################
             Connect-AzureAd -TenantId $tenantId -Credential $psCred -InformationAction Ignore
             $datapacketUriOIDC=$datapacketUri+"/signin-oidc"      
             $deviceManagementUriOIDC=$deviceManagementUri+"/signin-oidc"   
             $replyURLList = @($datapacketUriOIDC,$deviceManagementUriOIDC);  
             Write-Host '', 'Configuring and setting the Azure AD reply URLs' -ForegroundColor Green
             Set-AzureADApplication -ObjectId $objectId -HomePage $datapacketUri -ReplyUrls $replyURLList -Verbose
-            #####################################################################
             # Get Access token for calling API
-            #####################################################################
             $accessToken=Get-AccessTokenAPI $tenantId $clientId $clientSecret
             Write-Host "Access token is " $accessToken -ForegroundColor Magenta
             #add sleep inorder to have API startup successfully
             Start-Sleep -s 10
-            #####################################################################
             # Call API to initialize cosmos DB with default starter IoT templates
             # This templates will be loaded in the data packet designer
-            #####################################################################
             $cosmosDBInitResult=CosmosDbInit $datapacketUri $accessToken  
     }
 }
