911f082b125eacb70042d38f105bd785725556d9
unknown <omanduri@sysgain.com>
2018-08-08 15:13:23 +0530

    updated

--- scripts/initializecosmosdb-wmha.ps1
+++ scripts/initializecosmosdb-wmha.ps1
@@ -27,6 +27,22 @@ workflow  container{
 
         [Parameter(Mandatory=$true)]
         [string]
+        $subscriptionId,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $cosmosDBAccountKey,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $cosmosDbAccountName,
+        
+        [Parameter(Mandatory=$true)]
+        [string]
+        $cosmosDbName,
+
+        [Parameter(Mandatory=$true)]
+        [string]
         $objectId
     )
 
@@ -38,56 +54,40 @@ workflow  container{
         $deviceManagementUri = $Using:deviceManagementUri
         $azureAccountName = $Using:azureAccountName
         $azurePassword = $Using:azurePassword
+        $subscriptionId = $Using:subscriptionId
+        $cosmosDBAccountKey = $Using:cosmosDBAccountKey
+        $cosmosDbAccountName = $Using:cosmosDbAccountName
+        $cosmosDbName = $Using:cosmosDbName
         $objectId = $Using:objectId
-        Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
-        $password = ConvertTo-SecureString $azurePassword -AsPlainText -Force
-        $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $password)
-        Login-AzureRmAccount -Credential $psCred 
-    function Get-AccessTokenAPI{
-        Write-Host "`nRequesting access token.." -ForegroundColor Green
-    
-        $tokenParams = @{
-          client_id=$clientId;
-          client_secret=$clientSecret;
-          resource=$clientId;
-          grant_type='client_credentials';
-          scope='openid'
-        }
-        $baseUri="https://login.microsoftonline.com/"+$tenantId+"/oauth2/token"
-        $response=Invoke-RestMethod -Uri $baseUri -Method POST -Body $tokenParams
-     
-        return $response.access_token
-    }
-    function CosmosDbInit{
-    param([string]$apiBaseURL,[string]$accessToken)
-    
-        Write-Host "`nInitializing Cosmos DB..." -ForegroundColor Green
-        $APIURL=[string]::Format("{0}/api/templates/generate",$apiBaseURL)
-        $result=Invoke-RestMethod -Uri $APIURL -Method Post -ContentType "application/json" -Headers @{ "Authorization" = "Bearer $accessToken" }
-        if($result -eq "true")
-        {
-            Write-Host "`nCosmos DB initialized successfully..!" -ForegroundColor Green    
-            return $true;
-        }
-        else
-        {
-            Write-Host "`nError !! Cosmos DB not initialized!" -ForegroundColor Red  
-            return $false;  
-        }
-    }
-            # Update Azure AD applications reply urls
-            Connect-AzureAd -TenantId $tenantId -Credential $psCred -InformationAction Ignore
-            $deviceManagementUriOIDC="http://"+$deviceManagementUri+"/signin-oidc"
-            $replyURLList = @($deviceManagementUriOIDC);  
-            Write-Host '', 'Configuring and setting the Azure AD reply URLs' -ForegroundColor Green
-            Set-AzureADApplication -ObjectId $objectId -HomePage $deviceManagementUri -ReplyUrls $replyURLList -Verbose
-            # Get Access token for calling API
-            $accessToken=Get-AccessTokenAPI $tenantId $clientId $clientSecret
-            Write-Host "Access token is " $accessToken -ForegroundColor Magenta
-            #add sleep inorder to have API startup successfully
-            Start-Sleep -s 10
-            # Call API to initialize cosmos DB with default starter IoT templates
-            # This templates will be loaded in the data packet designer
-            $cosmosDBInitResult=CosmosDbInit $deviceManagementUri $accessToken  
-    }
+
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
+    $password = ConvertTo-SecureString $azurePassword -AsPlainText -Force
+    $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $password)
+    start-Sleep -s 20
+    Login-AzureRmAccount -TenantId $tenantId -SubscriptionID $subscriptionId -Credential $psCred 
+    start-Sleep -s 20
+
+    $primaryKey = ConvertTo-SecureString -String $cosmosDBAccountKey -AsPlainText -Force
+    $cosmosDbContext = New-CosmosDbContext -Account $cosmosDbAccountName -Database $cosmosDbName -Key $primaryKey
+    start-Sleep -s 20
+
+    # Create CosmosDB
+    New-CosmosDbDatabase -Context $cosmosDbContext -Id $cosmosDbName
+    start-Sleep -s 30
+
+    # Create CosmosDB Collections
+    New-CosmosDbCollection -Context $cosmosDbContext -Id 'Messages'  -OfferThroughput 400
+    start-Sleep -s 20
+    New-CosmosDbCollection -Context $cosmosDbContext -Id 'Templates' -OfferThroughput 400
+    start-Sleep -s 20
+    New-CosmosDbCollection -Context $cosmosDbContext -Id 'Groups' -OfferThroughput 400
+
+
+    #Update Azure AD applications reply urls
+    Connect-AzureAd -TenantId $tenantId -Credential $psCred -InformationAction Ignore
+    $deviceManagementUriOIDC=$deviceManagementUri+"/signin-oidc"
+    $replyURLList = @($deviceManagementUriOIDC);  
+    Write-Host '', 'Configuring and setting the Azure AD reply URLs' -ForegroundColor Green
+    Set-AzureADApplication -ObjectId $objectId -HomePage $deviceManagementUri -ReplyUrls $replyURLList -Verbose
+}
 }
