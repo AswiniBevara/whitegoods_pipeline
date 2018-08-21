0d8f278fd31bfd6f8816edf33ac19408e2d1f216
unknown <omanduri@sysgain.com>
2018-08-21 16:56:02 +0530

    updated

--- scripts/initializecosmosdb.ps1
+++ scripts/initializecosmosdb.ps1
@@ -27,11 +27,19 @@ workflow  container{
 
         [Parameter(Mandatory=$true)]
         [string]
-        $objectId,
+        $cosmosDBAccountKey,
 
         [Parameter(Mandatory=$true)]
         [string]
-        $datapacketUri
+        $cosmosDbAccountName,
+        
+        [Parameter(Mandatory=$true)]
+        [string]
+        $cosmosDbName,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $objectId
     )
 
     InlineScript{
@@ -41,25 +49,50 @@ workflow  container{
         $clientSecret = $Using:clientSecret
         $deviceManagementUri = $Using:deviceManagementUri
         $azureAccountName = $Using:azureAccountName
-        $azurePassword = $Using:azurePassword   
+        $azurePassword = $Using:azurePassword
+        $cosmosDBAccountKey = $Using:cosmosDBAccountKey
+        $cosmosDbAccountName = $Using:cosmosDbAccountName
+        $cosmosDbName = $Using:cosmosDbName
         $objectId = $Using:objectId
-        $datapacketUri=$Using:datapacketUri
+        Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
+        $password = ConvertTo-SecureString $azurePassword -AsPlainText -Force
+        $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $password)
+        Login-AzureRmAccount -Credential $psCred 
 
-    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
-    $password = ConvertTo-SecureString $azurePassword -AsPlainText -Force
-    $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $password)
-    start-Sleep -s 20
-    Login-AzureRmAccount -TenantId $tenantId -Credential $psCred 
+        start-Sleep -s 20
+
+    $primaryKey = ConvertTo-SecureString -String $cosmosDBAccountKey -AsPlainText -Force
+    $cosmosDbContext = New-CosmosDbContext -Account $cosmosDbAccountName -Database $cosmosDbName -Key $primaryKey
     start-Sleep -s 20
 
-    #Update Azure AD applications reply urls
-    Connect-AzureAd -TenantId $tenantId -Credential $psCred -InformationAction Ignore
-    $datapacketHomeUri="https://"+$datapacketUri
-    $datapacketUriOIDC="https://"+$datapacketUri+"/signin-oidc"      
-    $deviceManagementUriOIDC=$deviceManagementUri+"/signin-oidc"   
-    $replyURLList = @($datapacketUriOIDC,$deviceManagementUriOIDC);  
-    Write-Host '', 'Configuring and setting the Azure AD reply URLs' -ForegroundColor Green
-    Set-AzureADApplication -ObjectId $objectId -HomePage $datapacketHomeUri -ReplyUrls $replyURLList -Verbose
-    Start-Sleep -s 30
-}
+    # Create CosmosDB
+    New-CosmosDbDatabase -Context $cosmosDbContext -Id $cosmosDbName
+    start-Sleep -s 30
+
+    # Create CosmosDB Collections
+    New-CosmosDbCollection -Context $cosmosDbContext -Id 'Messages'  -OfferThroughput 400
+    start-Sleep -s 20
+    New-CosmosDbCollection -Context $cosmosDbContext -Id 'Templates' -OfferThroughput 400
+    start-Sleep -s 20
+    New-CosmosDbCollection -Context $cosmosDbContext -Id 'Groups' -OfferThroughput 400
+    start-Sleep -s 30
+  
+            # Update Azure AD applications reply urls
+            Connect-AzureAd -TenantId $tenantId -Credential $psCred -InformationAction Ignore
+         
+                 
+            $deviceManagementUriOIDC=$deviceManagementUri+"/signin-oidc"   
+            $replyURLList = @($deviceManagementUriOIDC);  
+            Write-Host '', 'Configuring and setting the Azure AD reply URLs' -ForegroundColor Green
+            Set-AzureADApplication -ObjectId $objectId -HomePage $deviceManagementUri -ReplyUrls $replyURLList -Verbose
+            # Get Access token for calling API
+            ##$accessToken=Get-AccessTokenAPI $tenantId $clientId $clientSecret
+            ##Write-Host "Access token is " $accessToken -ForegroundColor Magenta
+            #add sleep inorder to have API startup successfully
+            ##Start-Sleep -s 10
+            # Call API to initialize cosmos DB with default starter IoT templates
+            # This templates will be loaded in the data packet designer
+            ##$cosmosDBInitResult=CosmosDbInit $datapacketUri $accessToken  
+            Start-Sleep -s 30
+    }
 }
