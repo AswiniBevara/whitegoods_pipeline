20da8d2872e118fe162d0ed7bc86990d6bf5fa54
unknown <omanduri@sysgain.com>
2018-08-21 11:39:39 +0530

    updated

--- scripts/initializecosmosdb.ps1
+++ scripts/initializecosmosdb.ps1
@@ -27,18 +27,6 @@ workflow  container{
 
         [Parameter(Mandatory=$true)]
         [string]
-        $cosmosDBAccountKey,
-
-        [Parameter(Mandatory=$true)]
-        [string]
-        $cosmosDbAccountName,
-        
-        [Parameter(Mandatory=$true)]
-        [string]
-        $cosmosDbName,
-
-        [Parameter(Mandatory=$true)]
-        [string]
         $objectId
     )
 
@@ -49,10 +37,7 @@ workflow  container{
         $clientSecret = $Using:clientSecret
         $deviceManagementUri = $Using:deviceManagementUri
         $azureAccountName = $Using:azureAccountName
-        $azurePassword = $Using:azurePassword
-        $cosmosDBAccountKey = $Using:cosmosDBAccountKey
-        $cosmosDbAccountName = $Using:cosmosDbAccountName
-        $cosmosDbName = $Using:cosmosDbName
+        $azurePassword = $Using:azurePassword   
         $objectId = $Using:objectId
 
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
@@ -61,22 +46,6 @@ workflow  container{
     start-Sleep -s 20
     Login-AzureRmAccount -TenantId $tenantId -Credential $psCred 
     start-Sleep -s 20
-
-    $primaryKey = ConvertTo-SecureString -String $cosmosDBAccountKey -AsPlainText -Force
-    $cosmosDbContext = New-CosmosDbContext -Account $cosmosDbAccountName -Database $cosmosDbName -Key $primaryKey
-    start-Sleep -s 20
-
-    # Create CosmosDB
-    New-CosmosDbDatabase -Context $cosmosDbContext -Id $cosmosDbName
-    start-Sleep -s 30
-
-    # Create CosmosDB Collections
-    New-CosmosDbCollection -Context $cosmosDbContext -Id 'Messages'  -OfferThroughput 400
-    start-Sleep -s 20
-    New-CosmosDbCollection -Context $cosmosDbContext -Id 'Templates' -OfferThroughput 400
-    start-Sleep -s 20
-    New-CosmosDbCollection -Context $cosmosDbContext -Id 'Groups' -OfferThroughput 400
-    start-Sleep -s 30
 
     #Update Azure AD applications reply urls
     Connect-AzureAd -TenantId $tenantId -Credential $psCred -InformationAction Ignore
