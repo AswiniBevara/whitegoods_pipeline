0cbef77ae06645cdf22cdbf112d10d89acc74983
komali <komalio@sysgain.com>
2018-08-21 14:37:56 +0530

    Create adreplyurl.ps1

---
+++ scripts/adreplyurl.ps1
@@ -0,0 +1,70 @@
+workflow  container{
+    param(
+       
+        [Parameter(Mandatory=$true)]
+        [string]
+        $tenantId,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $clientId,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $clientSecret,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $datapacketUri,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $deviceManagementUri,
+        
+        [Parameter(Mandatory=$true)]
+        [string]
+        $azureAccountName,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $azurePassword,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $objectId
+    )
+
+    InlineScript{
+    
+        $tenantId = $Using:tenantId
+        $clientId = $Using:clientId
+        $clientSecret = $Using:clientSecret
+        $datapacketUri = $Using:datapacketUri
+        $deviceManagementUri = $Using:deviceManagementUri
+        $azureAccountName = $Using:azureAccountName
+        $azurePassword = $Using:azurePassword
+        $objectId = $Using:objectId
+        Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
+        $password = ConvertTo-SecureString $azurePassword -AsPlainText -Force
+        $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $password)
+        Login-AzureRmAccount -Credential $psCred 
+  
+            # Update Azure AD applications reply urls
+            Connect-AzureAd -TenantId $tenantId -Credential $psCred -InformationAction Ignore
+            $datapacketHomeUri="https://"+$datapacketUri
+            $datapacketUriOIDC="https://"+$datapacketUri+"/signin-oidc"      
+            $deviceManagementUriOIDC=$deviceManagementUri+"/signin-oidc"   
+            $replyURLList = @($datapacketUriOIDC,$deviceManagementUriOIDC);  
+            Write-Host '', 'Configuring and setting the Azure AD reply URLs' -ForegroundColor Green
+            Set-AzureADApplication -ObjectId $objectId -HomePage $datapacketHomeUri -ReplyUrls $replyURLList -Verbose
+            # Get Access token for calling API
+            ##$accessToken=Get-AccessTokenAPI $tenantId $clientId $clientSecret
+            ##Write-Host "Access token is " $accessToken -ForegroundColor Magenta
+            #add sleep inorder to have API startup successfully
+            ##Start-Sleep -s 10
+            # Call API to initialize cosmos DB with default starter IoT templates
+            # This templates will be loaded in the data packet designer
+            ##$cosmosDBInitResult=CosmosDbInit $datapacketUri $accessToken  
+            Start-Sleep -s 30
+    }
+}
