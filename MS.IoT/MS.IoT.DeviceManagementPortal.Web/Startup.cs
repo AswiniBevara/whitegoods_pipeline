f02796b94e4b7a68dbcb24af9d1927e82e211f8a
sudheermareddy <sudheerm@sysgain.com>
2018-10-05 11:04:10 +0530

    Update Startup.cs

--- MS.IoT/MS.IoT.DeviceManagementPortal.Web/Startup.cs
+++ MS.IoT/MS.IoT.DeviceManagementPortal.Web/Startup.cs
@@ -27,9 +27,22 @@ namespace MS.IoT.DeviceManagementPortal.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+
+            var builder = new ConfigurationBuilder()
+    .SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+            Configuration = builder.Build();
+
         }
 
         public IConfiguration Configuration { get; }
@@ -94,6 +107,9 @@ namespace MS.IoT.DeviceManagementPortal.Web
             //Quartz
             services.AddScoped<QuartzRefreshDBJob>();
             services.AddQuartz();
+
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
